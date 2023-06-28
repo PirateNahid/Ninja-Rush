9d54ef02e01f2bbf56329385acdab6807a59b2dc
PirateNahid <nhdnahid2022@gmail.com>
2023-06-28 14:39:29 +0600

    okay

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,21 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var velocity = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	$animation.play("run")
+	velocity.x = 50
+	move_and_slide(velocity)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Button_button_down():
-	get_tree().change_scene("res://scene/game.tscn")
-	pass # Replace with function body.
